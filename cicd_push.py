93bcb4e0ca1cc71953c0db5c617550157af0cb83
TracyWel <61886975+TracyWel@users.noreply.github.com>
2023-07-27 17:03:53 +0200

    good progress so far

--- cicd_push.py
+++ cicd_push.py
@@ -11,6 +11,7 @@ sftp to VM
 """
 import os
 import re
+import shutil
 
 
 def read_conf_file(config_path: str):
@@ -37,7 +38,8 @@ def read_conf_file(config_path: str):
             if file_str[-1] == "*":
                 main_file = file_str[:-1]
                 file_list.append(main_file)
-            file_list.append(file_str)
+            else:
+                file_list.append(file_str)
             print(file_str)
     # retrieve version number
     main_f = os.path.join(config_path, main_file)
@@ -49,3 +51,114 @@ def read_conf_file(config_path: str):
                 version_str = re.sub("[' ]", '', ver_str_list[1])
 
     return file_list, version_str
+
+
+def process_prod_line(line, p_str):
+    clean_line = line.rstrip()
+    line_list = []
+    # find number of relevant lines
+    index = clean_line.find(p_str)
+    prod_split_list = clean_line[index:].split('_')
+    num_lines = int(prod_split_list[1])
+    # line_list.append(clean_line)
+    if num_lines > 1:
+        for i in range(num_lines - 1):
+            # TODO: flesh this out later
+            # read line
+            # append to list
+            pass
+
+    if num_lines > 1:
+        # TODO: flesh this out later
+        pass
+    else:
+        # remove CICD flag
+        temp_line = re.sub(r'  # CICD:production_\d+', '', clean_line)
+        bare_line = f'{temp_line}\n'
+
+        if '#' in bare_line:
+            uncommented_line = re.sub(r'# ', '', bare_line)
+        else:
+            uncommented_line = bare_line
+
+    return uncommented_line
+
+
+# def process_py_files_for_vm():
+def process_py_files_for_vm(deployment_path, py_files):
+    """
+    test case
+    # db_file = 'fleet_manager.db'  # CICD:production_1
+    db_file = 'fleet_manager_e2etest_large.db' # CICD:testing_1
+
+    :return:
+    """
+    print('\nprocess_py_files_for_vm\n')
+    line_1 = "    # db_file = 'fleet_manager.db'  # CICD:production_1"
+    line_2 = "db_file = 'fleet_manager_e2etest_large.db' # CICD:testing_1"
+    prod_str = 'CICD:production'
+    testing_str = 'CICD:testing'
+    for py_file in py_files:
+            p_file = os.path.join(deployment_path, py_file)
+
+            if prod_str in open(p_file).read():
+                print(py_file)
+                py_file_out = f'new_{py_file}'
+                p_file_out = os.path.join(deployment_path, py_file_out)
+
+                with open(p_file_out, 'w') as pfo:
+
+                    with open(p_file, 'r') as pf:
+                        for line in pf:
+                            write_line = True
+                            ready_line = line
+
+                            # generate production line
+                            if prod_str in line:
+                                ready_line = process_prod_line(line, prod_str)
+                                print(ready_line)
+                                write_line = True
+
+                            # skip testing lines
+                            if testing_str in line:
+                                write_line = False
+
+                            if write_line:
+                                pfo.write(ready_line)
+
+
+def cicd_push_main(project_path):
+    """
+    Prepares code and pushes it to the VM for deployment.
+
+    1. copy relevant code to deploy folder, based on list in config file
+    2. process code (change paths mostly)
+    3. create zip file
+    4. sftp to VM
+
+    :return:
+    """
+    # project_path will be defined by arg parse
+
+    # process files
+    # zip
+    # sftp files
+
+    # check existence of deploy folder
+    deploy_folder = 'Deploy'
+    deploy_path = os.path.join(project_path, deploy_folder)
+    if not os.path.exists(deploy_path):
+        os.makedirs(deploy_path)
+
+    # read config file
+    list_files, version = read_conf_file(project_path)
+
+    # copy files to deploy folder
+    for f in list_files:
+        file_to_copy = os.path.join(project_path, f)
+        shutil.copy(file_to_copy, deploy_path)
+
+    # process file changes
+    process_py_files_for_vm(deploy_path, list_files)
+
+    return deploy_path
--- test/test_cicd_push.py
+++ test/test_cicd_push.py
@@ -1,9 +1,22 @@
 import pytest
-from cicd_push import read_conf_file
+import os
+from cicd_push import cicd_push_main, read_conf_file, process_py_files_for_vm
 
+
+def test_cicd_push_main():
+    proj_path = "C:/Users/TracyTD/OneDrive - Truth Data Insights/TD/Software/Flight_File_Handler"
+    deploy_path_str = cicd_push_main(proj_path)
+    assert os.path.exists(deploy_path_str)
 
 def test_read_conf_file():
     conf_path = "C:/Users/TracyTD/OneDrive - Truth Data Insights/TD/Software/Flight_File_Handler"
     list_files, version = read_conf_file(conf_path)
     assert list_files[0] == "data_gathering_utilities.py"
     assert version == '01.00.02'
+
+def test_process_py_files_for_vm():
+    conf_path = "C:/Users/TracyTD/OneDrive - Truth Data Insights/TD/Software/Flight_File_Handler"
+    deploy_path = "C:/Users/TracyTD/OneDrive - Truth Data Insights/TD/Software/Flight_File_Handler/Deploy"
+    list_files, version = read_conf_file(conf_path)
+    r_line = process_py_files_for_vm(deploy_path, list_files)
+    # assert r_line == "    db_file = 'fleet_manager.db'"
