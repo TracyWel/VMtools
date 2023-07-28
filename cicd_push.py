bd31021c35afde432d6116a7efb749494708660e
TracyWel <61886975+TracyWel@users.noreply.github.com>
2023-07-28 14:50:47 +0200

    first version in the wild!

--- cicd_push.py
+++ cicd_push.py
@@ -1,17 +1,73 @@
+__author__ = 'Tracy Welterlen'
+__copyright__ = 'Copyright 2023, Truth Data Insights'
+__credits__ = 'Tracy Welterlen'
+__version__ = '00.00.01'
+__maintainer__ = 'Tracy Welterlen'
+__email__ = 'tracy@truthdata.net'
+__status__ = 'Development'
 """
-create conf file with list of .py files that sits in project folder, ID main file
-CLI code that takes project folder path as input
-reads conf file
-makes Deploy folder, if not existing, copies files in list to Deploy
-edits code, based on CICD notes to change paths, etc for VM
-pulls the version number from main file
-zips up code
-sftp to VM
+This is part of a homemade Continous Integration / Continuous Deployment (CICD)
+system. It prepares the necessary files for a code to be deployed to the 
+production system.
+
+It uses a configuration file, called cicd.conf, with a list of .py files 
+that sits in project folder. The main code file is identified with .py*
+Any lines in cicd.conf starting with # are ignored.
+
+The CLI takes the project folder path as input then:
+1) reads the configuration file
+2) makes Deploy folder, if not existing, and copies files in list to Deploy
+3) edits code, based on CICD notes to change paths (see doc string for 
+process_prod_line), etc for VM
+4) pulls the version number from main file
+5) zips up code
+6) sftps the zip file to VM
 
 """
 import os
+import sys
 import re
 import shutil
+from typing import List
+import logging
+from argparse import ArgumentParser
+import os
+import zipfile
+
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+
+
+def parse_command_arguments(argv):
+    """
+    Parses CLI inputs
+    :param argv: command line arguments
+    :return:
+    """
+    parser = ArgumentParser(
+        description="Prepares the necessary files for a code to be deployed "
+                    "to the production system.")
+    parser.add_argument(
+        "-i",
+        "--input_path",
+        required=True,
+        default=' ',
+        help="Path to the project files",
+        type=str
+    )
+    args = parser.parse_args()
+    logging.info('parse_command_arguments: Project path: %s', args.input_path)
+
+    if not args.input_path:
+        logging.info('parse_command_arguments: ERROR: required path not set %s',
+                     args.input_path)
+        sys.exit(-1)
+
+    if not os.path.exists(os.path.join(args.input_path, 'cicd_push.conf')):
+        logging.info('parse_command_arguments: ERROR: configuration file '
+                     'missing %s/cicd_push.conf', args.input_path)
+        sys.exit(-1)
+
+    return args
 
 
 def read_conf_file(config_path: str):
@@ -33,6 +89,7 @@ def read_conf_file(config_path: str):
                 continue
 
             file_str = line.rstrip('\n')
+            logging.info('read_conf_file: %s', file_str)
 
             # identify main file
             if file_str[-1] == "*":
@@ -40,7 +97,7 @@ def read_conf_file(config_path: str):
                 file_list.append(main_file)
             else:
                 file_list.append(file_str)
-            print(file_str)
+
     # retrieve version number
     main_f = os.path.join(config_path, main_file)
     with open(main_f) as mf:
@@ -53,7 +110,26 @@ def read_conf_file(config_path: str):
     return file_list, version_str
 
 
-def process_prod_line(line, p_str):
+def process_prod_line(line: str, p_str: str):
+    """
+    Modifies lines in python files that include specific comments.
+
+      # CICD:production_1
+    Lines with this at the end (two spaces before the #) will be prepared for
+    production use. The comment is removed, the line is uncommented out if that
+    is the case and a newline character is added. Leading indentation is preserved.
+
+    The number at the end of the comment indicates how many lines are
+    impacted, eg. multiple continuation lines. At the moment (20230728) this
+    feature is not coded.
+
+
+    :param line: the current line of code, identified as having the production
+    comment in it
+    :param p_str: the production comment:  # CICD:production_1
+    :return: the modified line of code
+    """
+
     clean_line = line.rstrip()
     line_list = []
     # find number of relevant lines
@@ -85,26 +161,28 @@ def process_prod_line(line, p_str):
 
 
 # def process_py_files_for_vm():
-def process_py_files_for_vm(deployment_path, py_files):
+def process_py_files_for_vm(deployment_path: str, py_files: List[str]):
     """
-    test case
-    # db_file = 'fleet_manager.db'  # CICD:production_1
-    db_file = 'fleet_manager_e2etest_large.db' # CICD:testing_1
+    Modifies python files to prepare for production.
+    Lines with  # CICD:production_1 are prepared for use.
+    Lines with  # CICD:testing_1 are deleted.
 
-    :return:
+    :param deployment_path: path to deployment folder
+    :param py_files: list of python files from cicd.conf
+    :return: none
     """
-    print('\nprocess_py_files_for_vm\n')
-    line_1 = "    # db_file = 'fleet_manager.db'  # CICD:production_1"
-    line_2 = "db_file = 'fleet_manager_e2etest_large.db' # CICD:testing_1"
     prod_str = 'CICD:production'
     testing_str = 'CICD:testing'
+    new_files = []
     for py_file in py_files:
             p_file = os.path.join(deployment_path, py_file)
 
             if prod_str in open(p_file).read():
-                print(py_file)
+                logging.info('process_py_files_for_vm: %s', py_file)
                 py_file_out = f'new_{py_file}'
                 p_file_out = os.path.join(deployment_path, py_file_out)
+                pair_list = [py_file, py_file_out]
+                new_files.append(pair_list)
 
                 with open(p_file_out, 'w') as pfo:
 
@@ -116,7 +194,7 @@ def process_py_files_for_vm(deployment_path, py_files):
                             # generate production line
                             if prod_str in line:
                                 ready_line = process_prod_line(line, prod_str)
-                                print(ready_line)
+                                logging.info('process_py_files_for_vm: %s', ready_line)
                                 write_line = True
 
                             # skip testing lines
@@ -126,23 +204,45 @@ def process_py_files_for_vm(deployment_path, py_files):
                             if write_line:
                                 pfo.write(ready_line)
 
+    # replace old with new files
+    for pair in new_files:
+        os.remove(os.path.join(deployment_path, pair[0]))
+        os.rename(os.path.join(deployment_path, pair[1]),
+                  os.path.join(deployment_path, pair[0]))
 
-def cicd_push_main(project_path):
+
+def zip_deploy(dest_path: str, source_dir: str):
+    """
+    Zip files for deploy
+
+    :param dest_path: location to write the zip file
+    :param source_dir: location of the files to be zipped
+    :return: none
+    """
+
+    zf = zipfile.ZipFile(os.path.join(dest_path, 'vm_deploy.zip'), "w")
+    for dirname, subdirs, files in os.walk(source_dir):
+        for filename in files:
+            file_zip = os.path.join(dirname, filename)
+            zf.write(file_zip, os.path.basename(file_zip))
+    zf.close()
+
+
+def cicd_push_main(argv):
     """
     Prepares code and pushes it to the VM for deployment.
 
-    1. copy relevant code to deploy folder, based on list in config file
+    1. copies relevant code to deploy folder, based on list in config file
     2. process code (change paths mostly)
     3. create zip file
     4. sftp to VM
 
     :return:
     """
-    # project_path will be defined by arg parse
+    logging.info('cicd_push_main: Begin processing')
 
-    # process files
-    # zip
-    # sftp files
+    command_line_arguments = parse_command_arguments(argv)
+    project_path = command_line_arguments.input_path
 
     # check existence of deploy folder
     deploy_folder = 'Deploy'
@@ -152,13 +252,24 @@ def cicd_push_main(project_path):
 
     # read config file
     list_files, version = read_conf_file(project_path)
+    logging.info('cicd_push_main: file list\n%s', list_files)
 
     # copy files to deploy folder
     for f in list_files:
         file_to_copy = os.path.join(project_path, f)
         shutil.copy(file_to_copy, deploy_path)
+    logging.info('cicd_push_main: files moved to deploy folder')
 
     # process file changes
     process_py_files_for_vm(deploy_path, list_files)
+    logging.info('cicd_push_main: files processed')
 
-    return deploy_path
+    # zip
+    zip_deploy(project_path, deploy_path)
+    logging.info('cicd_push_main: files zipped')
+
+    # sftp zip
+
+
+if __name__ == "__main__":
+    cicd_push_main(sys.argv[1:])
--- test/test_cicd_push.py
+++ test/test_cicd_push.py
@@ -1,6 +1,7 @@
 import pytest
 import os
-from cicd_push import cicd_push_main, read_conf_file, process_py_files_for_vm
+from cicd_push import cicd_push_main, read_conf_file, process_py_files_for_vm, \
+process_prod_line, zip_deploy
 
 
 def test_cicd_push_main():
@@ -8,15 +9,34 @@ def test_cicd_push_main():
     deploy_path_str = cicd_push_main(proj_path)
     assert os.path.exists(deploy_path_str)
 
+
 def test_read_conf_file():
     conf_path = "C:/Users/TracyTD/OneDrive - Truth Data Insights/TD/Software/Flight_File_Handler"
     list_files, version = read_conf_file(conf_path)
     assert list_files[0] == "data_gathering_utilities.py"
     assert version == '01.00.02'
 
+
+def test_process_prod_line():
+    """
+    test case
+    # db_file = 'fleet_manager.db'  # CICD:production_1
+    db_file = 'fleet_manager_e2etest_large.db' # CICD:testing_1
+    :return:
+    """
+    new_line = "    # db_file = 'fleet_manager.db'  # CICD:production_1 "
+    prod_str = 'CICD:production'
+    ready_line = process_prod_line(new_line, prod_str)
+    assert ready_line == "    db_file = 'fleet_manager.db'\n"
+
+
 def test_process_py_files_for_vm():
-    conf_path = "C:/Users/TracyTD/OneDrive - Truth Data Insights/TD/Software/Flight_File_Handler"
-    deploy_path = "C:/Users/TracyTD/OneDrive - Truth Data Insights/TD/Software/Flight_File_Handler/Deploy"
-    list_files, version = read_conf_file(conf_path)
-    r_line = process_py_files_for_vm(deploy_path, list_files)
-    # assert r_line == "    db_file = 'fleet_manager.db'"
+    dep_path = "C:/Users/TracyTD/OneDrive - Truth Data Insights/TD/Software/Flight_File_Handler/Deploy"
+    file_list = ['flight_file_handler.py']
+    process_py_files_for_vm(dep_path, file_list)
+
+
+def test_zip_deploy():
+    des_path = "C:/Users/TracyTD/OneDrive - Truth Data Insights/TD/Software/Flight_File_Handler"
+    dep_path = "C:/Users/TracyTD/OneDrive - Truth Data Insights/TD/Software/Flight_File_Handler/Deploy"
+    zip_deploy(des_path, dep_path)
